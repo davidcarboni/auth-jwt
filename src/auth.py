ba9adc50352f133f104b158c58c81cb12b25682f
David Carboni <david.carboni@ros.gov.uk>
2017-06-13 16:59:04 +0100

    Added public-key route and improved the home route.

--- src/auth.py
+++ src/auth.py
@@ -1,7 +1,9 @@
 import logging
 import datetime
-from flask import Flask
+from flask import Flask, jsonify
+from json import dumps
 from .token import sign, decode
+from src import key
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -9,12 +11,28 @@ app = Flask("auth", static_folder='static')
 
 
 @app.route('/')
-def hello_world():
-    now = str(datetime.datetime.now())
-    log.debug(now)
-    data = {'hello': 'world'}
-    token = sign(data)
-    log.debug("Token: " + str(token))
-    decoded = decode(token)
-    log.debug("Decoded: " + decoded)
-    return 'Hello, World: ' + now
+def home():
+    data = {
+        'hello': 'world',
+        'now': str(datetime.datetime.now())
+    }
+    log.debug("Data: " + dumps(data))
+    jwt = sign(data)
+    log.debug("Token: " + jwt.decode('UTF8'))
+    decoded = decode(jwt)
+    log.debug("Decoded: " + dumps(decoded))
+    return jsonify({
+        'data': data,
+        'jwt': jwt,
+        'decoded': decoded
+    })
+
+
+@app.route('/public-key')
+def public_key():
+    """Returns the public key of this instance.
+    The fields in the returned Json match what Github does for public keys.
+    e.g. see https://api.github.com/users/davidcarboni/keys
+    """
+    log.debug("Public key requested.")
+    return jsonify({'id': key.key_id(), 'key': key.public_key().decode("UTF8")})
