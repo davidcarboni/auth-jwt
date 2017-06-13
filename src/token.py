79262d41ae5baf6641437f1ac397a8899756c8c0
David Carboni <david.carboni@ros.gov.uk>
2017-06-13 17:00:10 +0100

    Cleaned up.

--- src/token.py
+++ src/token.py
@@ -1,16 +1,12 @@
-from jose import jwt
-from jose.constants import ALGORITHMS
-from src.key import private_key, public_key, _key_pair
-
-print("Private : " + str(private_key()))
-print("Public  : " + str(public_key()))
+import jwt
+from src.key import private_key, public_key
 
 
 def sign(data):
-    #jwt.encode(data, private_key(), algorithm=ALGORITHMS.RS256)
-    return jwt.encode(data, _key_pair, algorithm=ALGORITHMS.RS256)
+    """Generates a JWT containing the given data."""
+    return jwt.encode(data, private_key(), algorithm='ES256')
 
 
 def decode(token):
-    return jwt.decode(token, bytearray(public_key()))
-    #return jwt.decode(token, _key_pair.publickey())
+    """Recovers the data from the given JWT."""
+    return jwt.decode(token, public_key(), algorithms=['ES256'])
