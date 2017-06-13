aeb128699d2bf7a8f35806b64c64389977bd8e8c
David Carboni <david.carboni@ros.gov.uk>
2017-06-13 17:01:35 +0100

    Switched to elliptic curve and sorted it out.

--- src/key.py
+++ src/key.py
@@ -1,19 +1,29 @@
-import os
-from Crypto.PublicKey import RSA
+import uuid
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, PublicFormat, NoEncryption
 
-# The key pair for this instance
-_key_pair = RSA.generate(int(os.getenv("KEY_SIZE", 4096)))
+
+# The key pair for this instance (elliptic curve ED25519)
+_key = {
+    'id': uuid.uuid4(),
+    'key_pair': ec.generate_private_key(ec.SECP521R1(), default_backend())
+}
+
+
+def key_id():
+    return _key["id"]
 
 
 def private_key():
-    """Renders the private key in PEM format, unencrypted, using PKCS1.
-    This is useful for signing JWT tokens.
+    """Renders the private key in PEM format, unencrypted, in PKCS8 format.
+    This is used for signing JWT tokens.
     """
-    return _key_pair.exportKey('PEM')
+    return _key["key_pair"].private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
 
 
 def public_key():
-    """Renders the public key in PEM format using PKCS1.
-    This is useful for verifying JWT token signatures.
+    """Renders the public key in PEM format.
+    This is used for verifying JWT token signatures.
     """
-    return _key_pair.publickey().exportKey('PEM')
+    return _key["key_pair"].public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
