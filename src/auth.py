b8072d9e634fdf0ed47495c066ebb848b6c18b07
David Carboni <david.carboni@ros.gov.uk>
2017-06-14 17:47:39 +0100

    Rearanged, tweaked and cleaned up.

--- src/auth.py
+++ src/auth.py
@@ -3,11 +3,32 @@ import datetime
 from flask import Flask, jsonify
 from json import dumps
 from .token import sign, decode
-from src import key
+from .key import generate_key
+from .database import list_keys, get_key
+import jwt
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 app = Flask("auth", static_folder='static')
+
+
+@app.route('/sign-in', methods=['POST'])
+def sign_in():
+    token = sign({'user': 'david', 'role': 'dude'})
+    return jsonify(token)
+
+
+@app.route('/keys')
+def keys():
+    """Returns the public key of this instance.
+    The fields in the returned Json match what Github does for public keys.
+    e.g. see https://api.github.com/users/davidcarboni/keys
+    """
+    log.debug("Public keys requested.")
+    result = []
+    for _id in list_keys():
+        result.append({'id': _id, 'key': get_key(_id)})
+    return jsonify(result)
 
 
 @app.route('/')
@@ -17,27 +38,16 @@ def home():
         'now': str(datetime.datetime.now())
     }
     log.debug("Data: " + dumps(data))
-    jwt = sign(data)
-    log.debug("Token: " + jwt.decode('UTF8'))
-    decoded = decode(jwt)
+    token = sign(data)
+    log.debug("Token: " + token)
+    log.debug("Generating new key to simulate being a different instance..")
+    generate_key()
+    log.debug("Now decoding token..")
+    decoded = decode(token)
     log.debug("Decoded: " + dumps(decoded))
     return jsonify({
         'data': data,
-        'jwt': jwt.decode("UTF8"),
-        'decoded': decoded
+        'jwt': token,
+        'jwt header': jwt.get_unverified_header(token),
+        'jwt claims': decoded
     })
-
-
-@app.route('/sign-in', methods=['POST'])
-def sign_in():
-    return jsonify(sign({'user': 'david', 'role': 'dude'}).decode("UTF8"))
-
-
-@app.route('/public-key')
-def public_key():
-    """Returns the public key of this instance.
-    The fields in the returned Json match what Github does for public keys.
-    e.g. see https://api.github.com/users/davidcarboni/keys
-    """
-    log.debug("Public key requested.")
-    return jsonify({'id': key.key_id(), 'key': key.public_key().decode("UTF8")})
