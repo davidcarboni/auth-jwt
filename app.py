6f347314cd526747bf69f06a47ca1472788060c5
David Carboni <david.carboni@ros.gov.uk>
2017-06-16 15:14:38 +0100

    Configurable logging level.

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import os
 import logging
 from src.auth import app
 
-logging.basicConfig(level=logging.DEBUG)
+debug = bool(os.getenv("FLASK_DEBUG"))
+logging_level = logging.DEBUG if debug else logging.WARNING
+logging.basicConfig(level=logging_level)
+logging.getLogger(__name__).info("Debug is " + str(debug))
 
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=os.getenv("PORT", "5000"),
-        debug=os.getenv("FLASK_DEBUG")
+        debug=debug
     )
