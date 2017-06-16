f95c3a47027f3dd815017cbb656354a1a9eeb461
David Carboni <david.carboni@ros.gov.uk>
2017-06-16 13:29:09 +0100

    Updated to point at Heroku.

--- test_scripts/heroku.sh
+++ test_scripts/heroku.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo --- Listing keys:
-curl localhost:5000/keys
+curl rosauth.herokuapp.com/keys
 echo --- Generating a new key:
-curl localhost:5000/
+curl rosauth.herokuapp.com/
 echo --- Listing keys again:
-curl localhost:5000/keys
+curl rosauth.herokuapp.com/keys
 echo --- Logging in with Json:
-curl -X POST -H "content-type: application/json" -d '{"user_id": "martin", "password": "zecrets"}' localhost:5000/sign-in
+curl -X POST -H "content-type: application/json" -d '{"user_id": "martin", "password": "zecrets"}' rosauth.herokuapp.com/sign-in
 echo --- Logging in with form:
-SESSION_ID=`curl -d 'user_id=martin&password=zecrets' localhost:5000/sign-in`
+SESSION_ID=`curl -d 'user_id=martin&password=zecrets' rosauth.herokuapp.com/sign-in`
 echo Session ID: $SESSION_ID
-curl localhost:5000/token/$SESSION_ID
+curl rosauth.herokuapp.com/token/$SESSION_ID
