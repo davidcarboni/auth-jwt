c7f99ee06a956e28b0a12a0d7862eeca0778e567
David Carboni <david.carboni@ros.gov.uk>
2017-06-16 12:11:57 +0100

    Naming tweak.

--- src/session.py
+++ src/session.py
@@ -5,7 +5,7 @@ It's intended for a small percentage of javascript-disabled and/or older browser
 from .database import save_token, get_token
 
 
-def create_session(token):
+def create_session(jwt):
 
     """Creates a new session and returns the session ID.
     You can use the session ID to recover the given token.
@@ -13,7 +13,7 @@ def create_session(token):
     :param token: A JWT to be stored on behalf of a client that cannot store it itself.
     :return: A session ID suitable for passing to the client as a cookie value.
     """
-    return save_token(token)
+    return save_token(jwt)
 
 
 def get_token(session_id):
