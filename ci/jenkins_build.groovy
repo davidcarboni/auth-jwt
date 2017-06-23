ea1fa29a9059aa376be226c9666a03678b9c3ef1
David Carboni <david.carboni@ros.gov.uk>
2017-06-23 16:02:14 +0100

    Get into the Groovy, you've got to proovy you can work for me.

--- ci/jenkins_build.groovy
+++ ci/jenkins_build.groovy
@@ -18,7 +18,7 @@ def dockerBuild() {
     sh "sudo docker build --tag auth ."
 }
 
-def dockerBuild() {
+def dockerRun() {
     sh "sudo docker rm -f auth || echo No instance running."
     sh "sudo docker run -d --name auth -p 5000:5000 auth"
 }
