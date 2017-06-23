c68ebb6b5a9aa6985864c54cc16dafe1c8b4f3b9
David Carboni <david.carboni@ros.gov.uk>
2017-06-23 15:47:15 +0100

    Added a Docker bulid stage.

--- ci/jenkins_build.groovy
+++ ci/jenkins_build.groovy
@@ -3,4 +3,13 @@ node("slave") {
     stage("Checkout code") {
         checkout scm
     }
+
+    stage("Build container image") {
+        dockerBuild()
+    }
+
+def dockerBuild() {
+    sh "docker build --tag auth ."
+}
+
 }
