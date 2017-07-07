138a70527abb5aa10d9e2b8a0e287cc43fca6b69
David Carboni <david.carboni@ros.gov.uk>
2017-07-07 12:55:01 +0100

    Debugging..

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def default():
 
 @app.route('/sign-in', methods=['GET'])
 def form():
-    log.info(request.cookies)
+    log.info("Cookie is: " + request.cookies)
+    log.info("Setting cookie for domain: " + COOKIE_DOMAIN)
     return render_template('index.html',
                            discharges_url=service_url('discharges'),
                            securities_url=service_url('securities'),
